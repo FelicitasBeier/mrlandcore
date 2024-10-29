e0838b4368eb2e14094906c67511a1f9424f86eb
Felicitas <beier@pik-potsdam.de>
2024-10-29 16:53:52 +0100

    unit conversion split into correctLPJmL (general transformation based on units) and calcLPJmLtransform (for special cases applied to some variables only)

--- R/calcLPJmLtransform.R
+++ R/calcLPJmLtransform.R
@@ -61,143 +61,88 @@ calcLPJmLtransform <- function(version     = "lpjml5.9.5-m1", # nolint
   }
   ########## CONFIGURE READ END ##########
 
-  ########## UNIT TRANSFORMATION START ###############
-  # To-Do: I barely changed things here (or just by accident) - all of this has to be revised.
+  ########## DATA TRANSFORMATION START ###############
+  # The standard unit conversion (e.g., from gC/m^2 to tC/ha, from mm to m^3/ha)
+  # takes place in correctLPJmL
+  # The following transformations are for selected inputs (e.g., those that
+  # need to be transformed to tDM)
 
-  if (grepl("soilc|soilc_layer|litc|vegc|alitfallc|alitter|vegc_grass|litc_grass|soilc_grass", subtype)) {
+  # To Do (Kristine): Please double-check the units of new inputs.
+  # (1) There are some that get converted to tC/ha, but not furhter to tDM/ha:
+  # (1.1) tC/ha: firec, litburnc, litc, litfallc, npb
+  # If any of these is supposed to be in tDM/ha, please add in the if below!
+  # (1.2) m^3/ha: pet is transformed as all other water inputs (from mm to m^3/ha).
+  # If you want a different unit, please add if below, but start with m^3/ha for transformation
+  # (2) Jan was against units like "ha/ha", "days of the year", "shr"
+  # For now all "" units in LPJmL got renamed to "1".
+  # If we need a different unit, we can change it either here (after discussion with Jan)
+  # or wherever the respective function is called.
 
-    unitTransform <- 0.01
-    x    <- x * unitTransform
-    unit <- "tC/ha"
-
-    if (grepl("litc|soilc_layer", subtype)) x <- toolConditionalReplace(x, "<0", 0)
-
-  } else if (grepl("*date*", subtype)) {
-
-    unit <- "day of the year"
-
-  } else if (grepl("aet|cft_transp_pft|discharge|runoff|lake_evap|input_lake", subtype)) {
-    # unit transformation
-    if (grepl("aet|cft_transp_pft", subtype)) {
-      # Annual evapotranspiration (evaporation + transpiration + interception) given in liter/m^2
-      # Plant transpiration in liter/m^2 per season
-      # Transform units: liter/m^2 -> m^3/ha
-      unitTransform <- 10
-      x             <- x * unitTransform
-
-    } else if (grepl("discharge", subtype)) {
-      # In LPJmL: (monthly) discharge given in hm3/d (= mio. m3/day)
-      # Transform units of discharge: mio. m^3/day -> mio. m^3/month
-      dayofmonths <- as.magpie(c(jan = 31, feb = 28, mar = 31, apr = 30, may = 31, jun = 30,
-                                 jul = 31, aug = 31, sep = 30, oct = 31, nov = 30, dec = 31))
-      x           <- x * dayofmonths
-
-      # Annual value (total over all month)
-      if (!grepl("^m", subtype)) {
-        x <- dimSums(x, dim = 3)
-      }
-
-    } else if (grepl("runoff", subtype)) {
-      ## In LPJmL: (monthly) runoff given in LPJmL: mm/month
-      # Transform units: liter/m^2 -> liter
-      # landarea in mio. ha
-      landarea <- setYears(collapseNames(dimSums(readSource("LUH2v2", subtype = "states",
-                                                            convert = "onlycorrect")[, "y1995", ],
-                                                 dim = 3)), NULL)
-      x        <- x * landarea * 1e10
-      # Transform units: liter -> mio. m^3
-      x <- x / (1000 * 1000000)
-
-      # Annual value (total over all month)
-      if (!grepl("^m", subtype)) {
-        x <- dimSums(x, dim = 3)
-      }
-
-    } else if (grepl("lake_evap|input_lake", subtype)) {
-      ## In LPJmL: given in mm (=liter/m^2)
-      # Multiply by lake share
-      lakeShare <- readSource("LPJmLInputs", subtype = "lakeshare", convert = "onlycorrect")
-      x          <- x * lakeShare
-
-      # Transform units: liter/m^2 -> liter
-      cb        <- toolGetMapping("LPJ_CellBelongingsToCountries.csv",
-                                  type = "cell", where = "mrcommons")
-      cellArea  <- (111e3 * 0.5) * (111e3 * 0.5) * cos(cb$lat / 180 * pi)
-      x         <- x * cellArea
-
-      # Transform units: liter -> mio. m^3
-      x <- x / (1000 * 1000000)
-
-      # Annual value (total over all month)
-      if (grepl("lake_evap", subtype)) {
-        x <- dimSums(x, dim = 3)
-      }
-    }
-
-    units <- c(aet            = "m^3/ha",
-               cft_transp_pft = "m^3/ha",
-               discharge      = "mio. m^3",
-               mdischarge     = "mio. m^3",
-               lake_evap      = "mio. m^3",
-               input_lake     = "mio. m^3",
-               runoff         = "mio. m^3",
-               mrunoff        = "mio. m^3")
-
-    unit <- toolSubtypeSelect(subtype, units)
-
-  } else if (grepl("*harvest*|gpp_grass", subtype)) {
-
-    yieldTransform <- 0.01 / 0.45
-    x    <- x * yieldTransform
+  if (grepl("gpp", "npp", "harvestc")) {
+    # Transformation from carbon to dry matter content
+    # (from tC/ha to tDM/ha)
+    x    <- x / 0.45
     unit <- "tDM/ha"
+  } else if (grepl("runoff")) {
+    # To Do (Feli): Decide whether this can be moved to mrwater
+    # and if so: move it.
 
-  } else if (grepl("irrig|cwater_b", subtype)) {
-    # Transform units: liter/m^2 (= mm) -> m^3/ha
-    irrigTransform  <- 10
-    # select only irrigated
-    x                <- x[, , "irrigated"] * irrigTransform # units are now: m^3 per ha per year
-    unit             <- "m^3/ha"
-
-  } else if (grepl("et_grass", subtype)) {
-    # Transform units: liter/m^2 (= mm) -> m^3/ha
-    watTransform <- 10
-    x            <- x * watTransform
-    unit         <- "m^3/ha"
-
-  } else if (grepl("input_lake", subtype)) {
-
+    ### Transformation to flow on land ###
+    # Transformation factor: 1 m^3/ha = 1e-6 mio. m^3/ha
+    # Transformation factor: 1 Mha    = 1e+6 ha
+    # landarea (in Mha)
+    landArea <- setYears(collapseNames(dimSums(readSource("LUH2v2",
+                                                          subtype = "states",
+                                                          convert = "onlycorrect")[, "y1995", ],
+                                               dim = 3)), NULL)
+    x <- x * landArea
+    # new unit
     unit <- "mio. m^3"
+  } else if (grepl("evap_lake")) {
+    # To Do (Feli): Decide whether this can be moved to mrwater
+    # and if so: move it.
+    # Note also: input_lake is now calculated from prec
+    # Probably better to do all of it in mrwater (calcRiverNaturalFlows)
 
+    ### Transformation to flow on water bodies ###
+    # lake area from LPJmL (in m^2 already transformed to ha)
+    # To Do (Feli): replace with LUH2v2 lake area
+    # Question (Jens): looks like LUH2v2 lake area includes ice?
+    # Question (Kristine, Pascal): how would I extract lake area from LUH2v2?
+    # should we do it during the LUH update?
+    lakeArea <- readSource("LPJmL", subtype = "lpjml5.9.5-m1:GSWP3-W5E5:historical:pnv:lake_area")
+    # transform from m^3/ha to mio. m^3
+    x <- x * lakeArea * 1e-6
+    # transform to yearly value
+    x <- dimSums(x, dim = "month")
+    ### Question (Jens): I don't think that we need this input at a monthly scale.
+    ### Do I overlook something? If not: can we get it as yearly output instead?
+    unit <- "mio. m^3"
+  } else if (grepl("*date*", subtype)) {
+    # To Do (Kristine and Jan): Discuss whether we want such renaming here
+    unit <- "day of the year"
   } else if (grepl("cshift", subtype)) {
-
+    # To Do (Kristine and Jan): Discuss whether we want such renaming here
     unit <- "C/C"
-
   } else if (grepl("fpc", subtype)) {
-
+    # To Do (Kristine and Jan): Discuss whether we want such renaming here
     unit <- "ha/ha"
-
-  } else if (grepl("mpet", subtype)) {
-
-    unit <- "mm/month"
-
   } else {
     stop(paste0("subtype ", subtype, " does not exist"))
   }
+  ########## DATA TRANSFORMATION STOP ###############
 
-  ########## UNIT TRANSFORMATION END ###############
-
-  ########## STAGE START  ###############
-
-  # TO-DO carify which calculation steps or test have to been done for the different stage arguments
+  ########## STAGE HANDELING START  ###############
+  # To Do carify which calculation steps or test have to been done for the different stage arguments
   if (grepl("smoothed", stage)) {
     x <- toolSmooth(x)
   } else if (grepl("raw", stage)) {
-    # unclear if checks are needed?
-
+    # unclear if checks are needed? I don't think so: should be covered in correctLPJmL already, right?
+    # To Do (discuss with Kristine, Mike, Feli, maybe Jan)
   } else {
     stop("Stage argument not supported!")
   }
-  ########## STAGE END    ###############
+  ########## STAGE HANDELING STOP  ###############
 
   return(list(x            = x,
               weight       = NULL,
--- R/correctLPJmL.R
+++ R/correctLPJmL.R
@@ -1,25 +1,65 @@
 #' @title correctLPJmL
-#' @description corrects data that was read in from LPJmL
-#' @param x magpie object provided by the read function
+#' @description corrects data that was read in from LPJmL and converts units
+#'              to MAgPIE standard
+#' @param x list of magpie object and unit provided by the read function
 #'
-#' @author Kristine Karstens, Felicitas Beier
+#' @author Felicitas Beier, Kristine Karstens
 #' @seealso
 #' [readLPJmL()]
 #' @examples
 #' \dontrun{
-#' readSource("LPJmL", convert = TRUE)
+#' readSource("LPJmL", convert = "onlycorrect")
 #' }
 #'
 
 correctLPJmL <- function(x) {
 
-  ### check whether there are negatives (and if so give warning)
+  # check and replace negative values
+  toolExpectTrue(x > 0, "Data provided by LPJmL is positive")
   x <- madrat::toolConditionalReplace(x, conditions = "<0", replaceby = 0)
 
-  ### do we want to check and correct N/As?
-  # Jens?, Kristine?, Mike?
+  # check and replace N/A's
+  toolExpectTrue(!is.na(x), "Data provided by LPJmL doesn't contain N/A's")
+  x <- toolConditionalReplace(x, conditions = c("is.na()"), replaceby = 0)
 
+  # extract unit of data
   unit <- madrat::getFromComment(x, "unit")
+
+  ### To Do (Kristine): please doubl-check unit conversion below
+
+  # unit conversion
+  if (grepl("gC/m2", unit)) {
+    # convert gC/m2 to tC/ha
+    # Conversion note:
+    # Transformation factor (numerator): 1 g = 1e-6 t
+    # Transformation factor (denominator): 1 m^2 = 1e-4 ha
+    x <- x * 0.01
+    unit <- "tC/ha"
+  } else if (grepl("mm", unit)) {
+    # convert mm to m^3/ha
+    # Conversion note:
+    # Def.: mm is equal to liter/m^2
+    # Transformation factor (numerator): 1 liter = 1e-3 m^3
+    # Transformation factor (denominator): 1 m^2   = 1e-4 ha
+    x <- x * 10
+    unit <- "m^3/ha"
+  } else if (identical(unit, "")) {
+    # empty units are replaced with 1 in magclass
+    unit <- "1"
+  } else if (unit == "m2") {
+    # convert m^2 to ha
+    # Transformation factor (numerator): 1 m^2 = 1e-4 ha
+    x <- x * 1e-4
+    unit <- "ha"
+    ### Question (Jan, Kristine): should we transform to ha
+    ### or does this include unnecessary rounding imprecision?
+    ### when we transform lake evap that also has to be multiplied with 1e-6
+    ### so that it ends up being to mio. m^3?
+  } else if (unit == "hm3/month") {
+    # Transformation factor (numerator): 1 cubic hectometer = 1 mio. cubic meter
+    ### Question (Jens, Kristine): Is this correct? What's hm3?
+    unit <- "mio. m^3/month"
+  }
 
   return(list(x = x,
               unit = unit))
