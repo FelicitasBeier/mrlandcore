b71dcb9f62b6d96c82c96826cad3cc48db995ebf
Felicitas <beier@pik-potsdam.de>
2024-11-06 22:52:36 +0100

    remove stage argument from calcLPJmLharmonize

--- R/calcLPJmLharmonize.R
+++ R/calcLPJmLharmonize.R
@@ -8,9 +8,6 @@
 #'                This argument can be used to split up the data in smaller objects
 #'                where only a sub-set of the data is needed or
 #'                for better handling where otherwise memory issues would occur due to the object size.
-#' @param stage degree of processing: harmonizedHistorical (until reference year of historical data)
-#'                                    harmonizedScenario (until reference year for harmonized Scenarios)
-#'              See toolLPJmLHarmonization for current settings.
 #'
 #' @return List of magpie objects with results on cellular level, weight, unit and description.
 #'
@@ -27,68 +24,63 @@
 #' }
 calcLPJmLharmonize <- function(version     = "lpjml5.9.5-m1", # nolint
                                climatetype = "MRI-ESM2-0:ssp370",
-                               subtype     = "pnv:soilc", subdata = NULL,
-                               stage       = "harmonizedScenario") {
-  #### To Do: get rid of stage argument!
-  #### Whenever historical: only smooth, whenever future scenario: harmonize
-  ## Extract argument information
+                               subtype     = "pnv:soilc", subdata = NULL) {
+  ### Question (Jan): calcLPJmLharmonize is very slow. Can we do anything to improve the performance?
 
-  # Create settings for LPJmL from version and climatetype argument
+  # Extract settings for LPJmL from version and climatetype argument
   cfg <- toolLPJmLHarmonization(version = version, climatetype = climatetype)
 
-  ### Question: calcLPJmLharmonize is very slow. Can we do anything to improve the performance?
+  if (grepl("historical", climatetype)) {
+    # return smoothed LPJmL data for historical baseline chosen
+    x <- calcOutput("LPJmLtransform", version = cfg$readinVersion,
+                    climatetype = cfg$climatetype, subtype = subtype,
+                    subdata = subdata, stage = "smoothed",
+                    aggregate = FALSE, supplementary = TRUE)
+    # extract unit from LPJmL data
+    unit <- x$unit
+    # extract data
+    x <- x$x
+  } else {
+    # The harmonization consists of two steps:
+    # (1) the reference scenario is harmonized to reference baseline
+    # based on the default settings of toolLPJmLHarmonization
 
-  if (stage == "harmonizedHistorical") {
     # read in historical data for subtype
-    baseline        <- calcOutput("LPJmLtransform", version = cfg$baselineVersion,
-                                  climatetype = cfg$baselineHist, subtype = subtype,
-                                  subdata = subdata, stage = "smoothed",
-                                  aggregate = FALSE, supplementary = TRUE)
-
-    unit            <- baseline$unit
-    baseline        <- baseline$x
+    baseline <- calcOutput("LPJmLtransform", version = cfg$baselineVersion,
+                           climatetype = cfg$baselineHist, subtype = subtype,
+                           subdata = subdata, stage = "smoothed",
+                           aggregate = FALSE, supplementary = TRUE)
+    unit <- baseline$unit
+    baseline <- baseline$x
 
     # read in future scenario data for subtype
-    x   <- calcOutput("LPJmLtransform", version = cfg$readinVersion,
-                      climatetype = cfg$climatetype, subtype = subtype,
-                      subdata = subdata, stage = "smoothed", aggregate = FALSE)
-    out <- toolHarmonize2Baseline(x, baseline, ref_year = cfg$refYearHist)
+    x <- calcOutput("LPJmLtransform", version = cfg$readinVersion,
+                    climatetype = cfg$baselineGcm, subtype = subtype,
+                    subdata = subdata, stage = "smoothed", aggregate = FALSE)
 
-  } else if (stage == "harmonizedScenario") {
-    # read in historical data for subtype
-    #### Kristine: something is wrong here: should this be a LPJmLharmonize call
-    ### or should the stage not be harmonized?
-    baselineScen    <- calcOutput("LPJmLharmonize", version = cfg$baselineVersion,
-                                  climatetype = cfg$baselineGcm, subtype = subtype,
-                                  subdata = subdata, stage = "harmonizedHistorical",
-                                  aggregate = FALSE, supplementary = TRUE)
+    # harmonize default future scenario to default baseline until
+    # default historical year
+    harmonizedScen <- toolHarmonize2Baseline(x, baseline, ref_year = cfg$refYearHist)
 
-    unit            <- baselineScen$unit
-    baselineScen    <- baselineScen$x
-
-    ### Kristine: What is this if-condition asking? Please comment
-    ### Is this the case where e.g. the climate scenario is GSWP3 (the same as the historical baseline)?
-    ### In this case it just returns the smoothed data, right? Does that mean that I don't need a
-    ### distinction such as in https://github.com/FelicitasBeier/mrwater/blob/HackathonLPJmL2MAgPIE/R/calcGrassET.R#L27
-    ### because the calcLPJmLharmonize handles it?
-    if (cfg$climatetype   == cfg$baselineGcm &&
+    # (2) the chosen climate scenario is harmonized to the reference scenario
+    if (cfg$climatetype == cfg$baselineGcm &&
           cfg$readinVersion == cfg$baselineVersion) {
-
-      out <- baselineScen
-
+      # If climatetype and baseline identical to the default climatetype
+      # and default baseline from toolLPJmLHarmonization
+      # no additional harmonization is required
+      out <- harmonizedScen
     } else {
-      #### Kristine: What's the difference to the case above ("harmonizedHistorical")?
-      ####           Why is the distinction necessary?
+      # Otherwise the scenario from the chosen climatetype is harmonized
+      # to the default climatescenario for the reference year where the
+      # GCM future starts
 
       # read in future scenario data for subtype
-      x   <- calcOutput("LPJmLtransform", version = cfg$readinVersion,
-                        climatetype = cfg$climatetype, subtype = subtype,
-                        subdata = subdata, stage = "smoothed", aggregate = FALSE)
-      out <- toolHarmonize2Baseline(x, baselineScen, ref_year = cfg$refYearGcm)
+      x <- calcOutput("LPJmLtransform", version = cfg$readinVersion,
+                      climatetype = cfg$climatetype, subtype = subtype,
+                      subdata = subdata, stage = "smoothed", aggregate = FALSE)
+      # harmonize chosen climate scenario to default baseline scenario
+      out <- toolHarmonize2Baseline(x, harmonizedScen, ref_year = cfg$refYearGcm)
     }
-
-  } else {
-    stop("Stage argument not supported!")
   }
 
   return(list(x            = out,
@@ -96,6 +88,6 @@ calcLPJmLharmonize <- function(version     = "lpjml5.9.5-m1", # nolint
               unit         = unit,
               min          = 0,
               description  = paste0("Output from LPJmL (", subtype, ") for ",
-                                    version, " and ", climatetype, " at stage: ", stage, "."),
+                                    version, " and ", climatetype, "."),
               isocountries = FALSE))
 }
