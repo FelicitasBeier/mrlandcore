a1c57d176f2406cf987c3415528c9c27ab91a8c9
FelicitasBeier <beier@pik-potsdam.de>
2026-03-04 17:49:27 +0100

    Croparea function bugfixes

--- R/calcCroparea.R
+++ R/calcCroparea.R
@@ -17,7 +17,7 @@
 #'
 #' @return areas of individual crops from FAOSTAT and weight
 #'
-#' @author Felicitas Beier
+#' @author Felicitas Beier, Benjamin Leon Bodirsky
 #'
 #' @importFrom magpiesets findset
 
@@ -39,50 +39,32 @@ calcCroparea <- function(sectoral = "kcr", physical = TRUE, cellular = FALSE,
                           "croparea from LandInG data set")
 
     # correction to match LUH cropland area
-    physCroparea <- calcOutput("CropareaLandInG", sectoral = sectoral, physical = TRUE,
-                               cellular = TRUE, irrigation = FALSE,
-                               selectyears = selectyears, aggregate = FALSE)
-    fallowLand   <- calcOutput("FallowLand", cellular = TRUE, aggregate = FALSE)
-    luh3Croparea <- calcOutput("LUH3", landuseTypes = "magpie", irrigation = FALSE,
-                               cellular = TRUE, yrs = selectyears, aggregate = FALSE)
+    physCroparea <- dimSums(calcOutput("CropareaLandInG", sectoral = sectoral, physical = TRUE,
+                                       cellular = TRUE, irrigation = FALSE,
+                                       selectyears = selectyears, aggregate = FALSE), dim = "crop")
+    fallowLand   <- calcOutput("FallowLand", cellular = TRUE, aggregate = FALSE)[, selectyears, ]
+    luh3 <- calcOutput("LUH3", landuseTypes = "magpie", irrigation = FALSE,
+                       cellular = TRUE, yrs = selectyears, aggregate = FALSE)
+    luh3Croparea <- collapseNames(luh3[, , "crop"])
+    luh3Total    <- dimSums(luh3, dim = "landuse")
 
-    mismatch <- dimSums(physCroparea, dim = 3) - luh3Croparea
-    ratio    <- luh3Croparea / physCroparea
+    #### BENNI: Please correct the correction!
+    correctionFactor <- ifelse((physCroparea + fallowLand) > 0 & luh3Croparea > 0,
+                               luh3Croparea / (physCroparea + fallowLand),
+                               1)
 
+    # apply correction to croparea from above
+    croparea <- croparea * correctionFactor
 
-
-    # fallow land + crop area = LUH cropland
-    # do this at cellular, then aggregate
-
-    # check whether physical croparea matches LUH total land area
-    # LUH3 landarea must >= physical crop area
-
-    # Check for landmass mismatch: Total physical croparea should be smaller than landmass
-    # Total physical croparea
-    physTotal <- dimSums(physCroparea, dim = 3)
-    # Total land area according to LUH3 in iniyear (constant over time)
-    landarea <- dimSums(luh3Croparea, dim = 3)
-
-    if (any(round(landarea - physTotal, digits = 6) < 0)) {
-      # Note: Due to mismatches in the land masks used in LandInG
-      #       and LUH, croparea may exceed total landarea.
-      stop("This should no longer be the case after the LandInG update.
-         Please check where mismatch is coming from starting from calcCropareaAdjusted.")
-      vcat(verbosity = 0,
-           paste0("In calcCropareaAdjusted: There is a mismatch in the landmask underlying
-                the ", dataset, " croparea dataset and LUH.
-                Croparea is cut here to fit into the landmass as reported by LUH,
-                but a more generic solution should be found to make the data consistent!"))
-
-      ratio <- ifelse(landarea - physTotal < 0,
-                      landarea / physTotal,
-                      1)
-      # Scale down crop-specific croparea by mismatch area
-      phys <- phys * ratio
-
+    # Sanity check (physical croparea + fallow land should not exceed total land area in LUH3)
+    if (any((physCroparea + fallowLand) - luh3Total > 0)) {
+      stop("Croparea is larger than total land area in LUH3.")
     }
-
-
+    # Sanity check (physical croparea + fallow land should not exceed cropland area in LUH3)
+    if (any((physCroparea + fallowLand) - luh3Croparea > 0)) {
+      stop("Croparea + fallow land is larger than cropland area in LUH3.
+            This should no longer be the case after the correction.")
+    }
 
     # Aggregation to iso-level
     if (!cellular) {
