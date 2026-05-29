8338aec5bd41b5b4b47ff20d4c93a436a63e223b
FelicitasBeier <beier@pik-potsdam.de>
2026-05-29 16:03:11 +0200

    fixes in calcMulticropping

--- R/calcMulticropping.R
+++ R/calcMulticropping.R
@@ -1,11 +1,11 @@
 #' @title calcMulticropping
 #' @description Multicropping factor, telling how many harvests are produced
-#' on one unit of phyiscal area. Fallow lands are not counted in, so the factor
+#' on one unit of physical area. Fallow lands are not counted in, so the factor
 #' is always larger than 1.
 #'
 #' @param irrigation report irrigated and non-irrigated areas separately
 #' @return List of magpie objects with results on country level, weight on country level, unit and description.
-#' @author Benjamin Leon Bodirsky
+#' @author Benjamin Leon Bodirsky, Felicitas Beier
 #' @seealso
 #' [calcCroparea()]
 #' @examples
@@ -13,15 +13,15 @@
 #' calcOutput("Multicropping")
 #' }
 #'
-calcMulticropping <- function(irrigation = TRUE, cellular = FALSE) {   # nolint
+calcMulticropping <- function(irrigation = TRUE) {   # nolint
 
-  phys   <- collapseNames(dimSums(calcOutput("Croparea", physical = TRUE, cellular = cellular,
+  phys   <- collapseNames(dimSums(calcOutput("Croparea", physical = TRUE, cellular = TRUE,
                                              aggregate = FALSE, irrigation = irrigation),
                                   dim = 3.1))
-  harv   <- collapseNames(dimSums(calcOutput("Croparea", physical = FALSE, cellular = cellular,
+  harv   <- collapseNames(dimSums(calcOutput("Croparea", physical = FALSE, cellular = TRUE,
                                              aggregate = FALSE, irrigation = irrigation),
                                   dim = 3.1))
-  out <- harv / phys
+  out <- ifelse(phys > 0, harv / phys, NA)
 
   out[is.na(out)] <- 1
 
